eed73d314f21c61b8982119ae32b57421d75ba31
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-01-12 17:30:26 +0100

    fonction jump

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
     //Composant
     private Rigidbody2D _rigidbody2D;
     private PlayerInput _playerInput;
+    private bool _isOnGround;
+    [SerializeField] Transform _groundPoint;
+    [SerializeField] LayerMask _WhatIsTheGround;
 
     //Moving
     [SerializeField] float _speed = 5f;
@@ -12,12 +15,15 @@ public class PlayerController : MonoBehaviour
     float _moveEndTime;
 
     //Jumping
-    [SerializeField] float _jumpForce = 5f;
+
     Transform _targetTranform;
     Transform _transform;
     private bool _yReached;
+    [SerializeField] Transform[] _PropsColl;
+
 
     public Rigidbody2D Rigidbody2D { get => _rigidbody2D; }
+    public Transform TargetTranform { get => _targetTranform; }
 
     private void Awake()
     {
@@ -31,44 +37,23 @@ public class PlayerController : MonoBehaviour
     {
 
     }
+
+
+
     private void FixedUpdate()
     {
         Walk();
+
+        TargetJumpPoint();
+
+
 
     }
     // Update is called once per frame
     void Update()
     {
-        if (_targetTranform != null )
-        {
-            if (Vector2.Distance(new Vector2(0f, _transform.position.y), new Vector2(0f, _targetTranform.position.y)) > 0.5f && !_yReached)
-            {
+        _isOnGround = Physics2D.OverlapCircle(_groundPoint.position, 0.3f, _WhatIsTheGround);
 
-                _transform.position = Vector2.MoveTowards(_transform.position, new Vector2(_transform.position.x, _targetTranform.position.y), Time.deltaTime * 8);
-            }
-            else if(_yReached == false )
-            {
-                _rigidbody2D.gravityScale = 1;
-                _yReached = true;
-            }
-
-            if (_yReached)
-            {
-                if (Vector2.Distance(new Vector2(_transform.position.x, 0f), new Vector2(_targetTranform.position.x, 0f)) > 0.2f)
-                {
-
-                    _transform.position = Vector2.MoveTowards(_transform.position, new Vector2(_targetTranform.position.x, _transform.position.y), Time.deltaTime * 8);
-
-                }
-                else
-                {
-                    _targetTranform = null;
-                    _yReached = false;
-                } 
-            }
-
-
-        }
     }
 
     #region WALK
@@ -90,15 +75,77 @@ public class PlayerController : MonoBehaviour
 
     #region JUMP
 
-    public void Jump()
+    public void Jump(int index)
     {
-        _targetTranform = GameObject.Find("JumpPoint").transform;
+        _targetTranform = _PropsColl[index -1];
         _rigidbody2D.gravityScale = 0;
-
-
-        //_rigidbody2D.velocity = Vector2.up * _jumpForce;
         Debug.Log("Player Jump");
+
+        //_interploateAmount = (_interploateAmount + Time.deltaTime) % _jumpForce;
+        //_pointAB.position = Vector3.Lerp(_pointA.position, _pointB.position, _interploateAmount);
+        //_pointBC.position = Vector3.Lerp(_pointB.position, _pointC.position, _interploateAmount);
+
+
+
     }
 
     #endregion
+
+    #region Target
+    public void TargetJumpPoint()
+    {
+        if (_targetTranform != null)
+        {
+            if (Vector2.Distance(new Vector2(0f, _transform.position.y), new Vector2(0f, _targetTranform.position.y)) > 0.2f && !_yReached)
+            {
+
+                _transform.position = Vector2.MoveTowards(_transform.position, new Vector2(_transform.position.x, _targetTranform.position.y), Time.deltaTime * 8);
+            }
+            else if (_yReached == false)
+            {
+                _rigidbody2D.gravityScale = 1;
+                _yReached = true;
+            }
+
+            if (_yReached)
+            {
+                if (Vector2.Distance(new Vector2(_transform.position.x, 0f), new Vector2(_targetTranform.position.x, 0f)) > 0.2f)
+                {
+
+                    _transform.position = Vector2.MoveTowards(_transform.position, new Vector2(_targetTranform.position.x, _transform.position.y), Time.deltaTime * 8);
+
+                }
+                else
+                {
+                    _targetTranform = null;
+                    _yReached = false;
+                }
+            }
+
+
+        }
+    }
+
+    #endregion
+
+
+    private void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (_isOnGround)
+        {
+            switch (Other.gameObject.tag)
+            {
+                case "PropsColl":
+                    Jump(1);
+                    break;
+                case "PropsColl2":
+                    Jump(2);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
 }
