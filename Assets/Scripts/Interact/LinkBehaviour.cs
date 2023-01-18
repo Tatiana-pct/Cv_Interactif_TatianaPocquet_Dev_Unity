5dc96f73c0b315f057e7c5e8f18a1e867291229c
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-01-18 10:03:59 +0100

    implementation of clickable links and change of the intercation cursor

--- Assets/Scripts/Interact/LinkBehaviour.cs
+++ Assets/Scripts/Interact/LinkBehaviour.cs
@@ -20,17 +20,7 @@ public class LinkBehaviour : MonoBehaviour
 
     private void OnMouseDown()
     {
-        //if (gameObject.CompareTag("Linkedin"))
-        //    {
-        //    Application.OpenURL("https://www.linkedin.com/in/tatiana-pocquet/");
-        //}
-
-        //if (gameObject.CompareTag("ENI"))
-        //{
-        //    Application.OpenURL("https://www.eni.fr/?gclid=Cj0KCQiAn4SeBhCwARIsANeF9DKLUEvZzgCU6dIkQNHmp_3Ee_ORF6bX6tX0e0fbvuFhuMkmI6cG3xUaAkNhEALw_wcB");
-        //    Debug.Log("clik");
-        //}
-
+        
         switch(gameObject.tag)
         {
             case "Linkedin":
@@ -50,6 +40,18 @@ public class LinkBehaviour : MonoBehaviour
                 break;
             case "3WA":
                 Application.OpenURL("https://3wa.fr/formations/formation-developpeur-web/developpeur-jeux-video-unity-3d-3-mois-temps-plein/");
+                break;
+            case "Udemy":
+                Application.OpenURL("https://www.udemy.com/");
+                break;
+            case "Dyma":
+                Application.OpenURL("https://dyma.fr/");
+                break;
+            case "YouTube":
+                Application.OpenURL("https://www.youtube.com/@TUTOUNITYFR");
+                break;
+            case "GameCodeur":
+                Application.OpenURL("https://www.gamecodeur.fr/toutes-les-categories/");
                 break;
         }
 
