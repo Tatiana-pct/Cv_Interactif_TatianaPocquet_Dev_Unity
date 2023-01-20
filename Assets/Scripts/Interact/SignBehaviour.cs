9c78db3c383ae9c05f569db3930204ee5455d1fd
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-01-20 13:54:19 +0100

    Update SignBehaviour.cs

--- Assets/Scripts/Interact/SignBehaviour.cs
+++ Assets/Scripts/Interact/SignBehaviour.cs
@@ -7,13 +7,12 @@ using UnityEngine;
 public class SignBehaviour : MonoBehaviour
 {
 
-    //[SerializeField] string _signText;
+   
     //Canvas des panneaux
     [SerializeField] GameObject _canvas;
     [SerializeField] TMP_Text _text;
 
-    //canvas d'interaction
-    //[SerializeField] GameObject _canvasToucheInteract;
+    
 
     private PlayerInput _playerInput;
 
@@ -26,7 +25,7 @@ public class SignBehaviour : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            //_canvasToucheInteract.SetActive(true);
+            
 
 
             _canvas.SetActive(true);
@@ -45,15 +44,12 @@ public class SignBehaviour : MonoBehaviour
 
     public void HideCanvas()
     {
-        //_canvasToucheInteract.SetActive(false);
+       
 
         _canvas.SetActive(false);
     }
 
-    public void ShowDial()
-    {
-
-    }
+    
 
 
 
